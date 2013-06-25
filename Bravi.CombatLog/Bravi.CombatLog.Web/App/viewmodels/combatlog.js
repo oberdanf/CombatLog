05d00620acbf98f9cebb2d197e1fc43e9bce107f
danillo corvalan <danillo.corvalan@gmail.com>
2013-06-25 17:24:00 -0300

    select winner inside combat log view

--- Bravi.CombatLog/Bravi.CombatLog.Web/App/viewmodels/combatlog.js
+++ Bravi.CombatLog/Bravi.CombatLog.Web/App/viewmodels/combatlog.js
@@ -2,6 +2,9 @@
     var currentPlayers = require('models/currentPlayers'),
         router = require('durandal/plugins/router');
 
+    //player number: 1 or 2
+    var playerWinnerNumber = ko.observable('');
+
     return {
         selectedPlayers: function() {
             var selectedFromCurrentPlayers = currentPlayers.getSelectedPlayers();
@@ -12,9 +15,7 @@
 
             return selectedFromCurrentPlayers;
         },
-        setWinner: function (player) {
-            alert(player.name + '\r\n' + player.email);
-        },
+        playerWinnerNumber: playerWinnerNumber,
         activate: function () {
             if (!this.selectedPlayers() || this.selectedPlayers().length !== 2) {
                 router.navigateTo('#/selectplayers');
