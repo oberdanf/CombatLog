aec7ec56cf5b085daa3a8bf38e2675542a250ddd
Danillo Corvalan <danillo.corvalan@gmail.com>
2013-06-25 23:39:11 -0300

    finished combat log round and save proccess js coding.

---
+++ Bravi.CombatLog/Bravi.CombatLog.Web/App/services/combatLogService.js
@@ -0,0 +1,11 @@
+﻿define(function() {
+    var saveCombat = function(player, round, flawlessVictory) {
+        var deferred = new $.Deferred();
+        
+        return deferred.resolve(player.name).promise();
+    };
+
+    return {
+        saveCombat: saveCombat
+    };
+});
