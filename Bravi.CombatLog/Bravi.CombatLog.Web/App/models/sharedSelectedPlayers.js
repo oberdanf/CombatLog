ddecbd23b8a3ef63413cbf6963642bee8adf112a
Danillo Corvalan <danillo.corvalan@gmail.com>
2013-06-30 14:50:04 -0300

    sharedSelectedPlayers and finished selectplayers view

---
+++ Bravi.CombatLog/Bravi.CombatLog.Web/App/models/sharedSelectedPlayers.js
@@ -0,0 +1,9 @@
+﻿define(function() {
+    var player1 = ko.observable('');
+    var player2 = ko.observable('');
+
+    return {
+        player1: player1,
+        player2: player2
+    };
+});
--- Bravi.CombatLog/Bravi.CombatLog.Web/App/viewmodels/selectplayers.js
+++ Bravi.CombatLog/Bravi.CombatLog.Web/App/viewmodels/selectplayers.js
@@ -1,24 +1,58 @@
 ﻿define(function (require) {
-    var currentPlayers = require('models/currentPlayers'),
+    var sharedSelectedPlayers = require('models/selectedPlayers'),
+        dataService = require('services/dataService'),
         router = require('durandal/plugins/router');
 
-    var startLog = function () {
-        if (currentPlayers.getSelectedPlayers().length > 1) {
-            router.navigateTo('#/combatlog');
-        } else {
-            alert('In order to start a log you must have at least two players!');
-        }
+    var selectablePlayer = function(player) {
+        var self = this;
+        self.player = player;
+        self.selected = ko.observable(false);
     };
-    
+
+    var playersToSelect = ko.observable();
+
+    var setPlayers = function () {
+        var deferred = new $.Deffered();
+        
+        dataService.getAllPlayers().done(getAllPlayersSuccess);
+        function getAllPlayersSuccess(playersReturned) {
+            var _players = [];
+            for (var i = 0; i < playersReturned.length; i++) {
+                _players.push(new selectablePlayer(playersReturned[i]));
+            }
+            playersToSelect(_players);
+            deferred.resolve();
+        };
+
+        return deferred.promise();
+    };
+
+    var goToSelectCharacters = function() {
+        var selectedPlayers = playersToSelect().filter(function(item) {
+            return item.selected() === true;
+        });
+        
+        if (selectedPlayers.length !== 2) {
+            throw new Error('You must have only two players');
+        }
+
+        sharedSelectedPlayers.player1(selectedPlayers[0]);
+        sharedSelectedPlayers.player2(selectedPlayers[1]);
+        
+        router.navigateTo('#/selectcharacters');
+    };
+
     return {
-        players: currentPlayers.getCurrentPlayers(),
-        selectedPlayers: currentPlayers.getSelectedPlayers,
+        playersToSelect : playersToSelect,
+        goToSelectCharacters: goToSelectCharacters,
         viewAttached: function () {
             $('body').trigger('create');
             setTimeout(function() {
                 $('body').trigger('create');
             }, 250);
         },
-        startLog: startLog
+        activate: function () {
+            return setPlayers();
+        },
     };
 });
